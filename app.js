9ddbf51ce596f850a904068150a1540cdbe973df
Julie Knowles <julie@clearlycoded.com>
2014-12-15 19:44:54 -0800

    trying stuff

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var app = require('./server/server.js');
+var app = require('./server/productionServer.js');
 
-var port = process.env.PORT || 8000;
+var port = 8000;
 app.listen(port, function() {
   console.log('Server is listening on ' + port);
 });
