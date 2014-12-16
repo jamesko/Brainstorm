01298bd5179514fa8554770417050abb57912bfc
Julie Knowles <julie@clearlycoded.com>
2014-12-15 21:21:03 -0800

    Added process.env.port to index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('./server/server.js');
 
-var port = 8000;
+var port = process.env.port || 8000;
 app.listen(port, function() {
   console.log('Server is listening on ' + port);
 });
