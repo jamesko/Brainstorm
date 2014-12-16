451940a0c4ea006400c9d2b912f9df48e23765ac
Julie Knowles <julie@clearlycoded.com>
2014-12-15 21:16:21 -0800

    Removed .module-cache directory

--- client/app/react/.module-cache/f7be0acbe6c63218b02c9fd5ff22fbc0f65064f0.js
+++
@@ -1,9 +0,0 @@
-app.CreateComment = React.createClass({displayName: 'CreateComment',
-  render: function(){
-    return (
-      React.createElement("div", {ref: "body"}, 
-        React.createElement(app.CommentForm, {idea_id: "5487b6bcd45728763106ce12"})
-      )
-    );
-  }
-});
