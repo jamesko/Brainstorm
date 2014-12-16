f6af25c91e67c3f1567036399d0f10adf2502cb0
Julie Knowles <julie@clearlycoded.com>
2014-12-15 19:00:02 -0800

    Updated app again

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var app = require('./server/productionServer.js');
 
 app.set('port', process.env.PORT || 8000);
-app.listen(port, function() {
-  console.log('Server is listening on ' + port);
+app.listen(port);
 });
