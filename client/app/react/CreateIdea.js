9bddb6e8179695d7bb6bac72c1e80f83a4cbfc64
Eric Kennedy <ekeric13@gmail.com>
2015-02-11 15:34:18 -0800

    Add client/lib and client/app/react to gitignore

--- client/app/react/CreateIdea.js
+++
@@ -1,9 +0,0 @@
-app.CreateIdea = React.createClass({displayName: 'CreateIdea',
-  render: function(){
-    return (
-      React.createElement("div", null, 
-        React.createElement(app.IdeaForm, {room_id: this.props.room_id})
-      )
-    );
-  }
-});
