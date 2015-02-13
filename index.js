941403063754fcff4bb77fdeb3073bd4162fff96
Eric Kennedy <ekeric13@gmail.com>
2015-02-13 10:46:57 -0800

    Change port to 8000

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('./server/server.js');
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 app.listen(port, function() {
   console.log('Server is listening on ' + port);
 });
