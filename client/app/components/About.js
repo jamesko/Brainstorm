063f45e31f853642a416259f0a3aef75814ddfaa
Eric Kennedy <ekeric13@gmail.com>
2015-02-24 13:03:07 -0800

    Add route and component for an about page

---
+++ client/app/components/About.js
@@ -0,0 +1,16 @@
+var React = require("react");
+
+var About = React.createClass({
+
+  render: function(){
+    return (
+      <div>
+        <h1>Here </h1>
+      </div>
+
+    );
+  }
+
+});
+
+module.exports = About;
