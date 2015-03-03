3228925c60fb83dc097a9a774e7f07032b6d19d9
Eric Kennedy <ekeric13@gmail.com>
2015-03-03 01:32:03 -0800

    Code cleanup

--- client/app/starter.js
+++ client/app/starter.js
@@ -27,38 +27,3 @@ var routes = (
 Router.run(routes, function (Handler) {
   React.render(<Handler/>, document.getElementById('main'));
 });
-
-//pay attention to rooms id
-
-// if (document.location.hash.substr(3, 5) === 'rooms'){
-//   PageActions.navigate({
-
-//     dest: 'rooms',
-//     props: document.location.hash.substr(9)
-
-//   });
-// } else {
-//   PageActions.navigate({
-
-//     dest: 'welcome'
-
-//   });
-// };
-
-// window.addEventListener ('popstate' , function (event) {
-//   console.log ('HISTORY' , window.history)
-//   var path = event.state.path.slice (1);
-//   var pathParts = path.split ('/');
-
-//   if (pathParts[0] === 'brainswarms') {
-
-//     BrainswarmStore.visitBrainswarm (pathParts[1]);
-//   }
-//   PageActions.navigate ({
-//     dest : pathParts[0] ,
-//     data : pathParts[1]
-//   });
-
-// });
-
-// possibly add a brainswarm in here for when user opens app on a brainswarm url
