51b7741a175ee8103516ff73a94b401582619a5c
Eric Kennedy <ekeric13@gmail.com>
2015-03-06 20:29:04 -0800

    Clean up unnecessary brainswarm store code after refactor

--- client/app/stores/BrainswarmStore.js
+++ client/app/stores/BrainswarmStore.js
@@ -3,37 +3,8 @@ var BrainswarmActions = require("../actions/BrainswarmActions");
 var socket = io.connect();
 
 var BrainswarmStore = Reflux.createStore({
-  _brainswarms: [],
 
   listenables: BrainswarmActions,
-
-  init: function() {
-    // this.socketListener();
-  },
-
-
-  getAll: function() {
-    return this._brainswarms;
-  },
-
-  socketListener: function(){
-    socket.on('brainswarm-change', function(currentBrainswarms) {
-      if (currentBrainswarms){
-        // this._brainswarms = currentBrainswarms;
-      }
-      this.trigger();
-    }.bind(this));
-  },
-
-  //checkBrainswarm: function(idea_id, callback){
-  //  var brainswarms = this._brainswarms;
-  //  for (var i =0; i < brainswarms.length; i++){
-  //    if (brainswarms[i].idea === idea_id){
-  //      return callback(brainswarms[i]);
-  //    }
-  //  }
-  //  callback();
-  //},
 
   getBrainswarm: function(idea_id, callback) {
     $.ajax({
@@ -41,19 +12,10 @@ var BrainswarmStore = Reflux.createStore({
       url: '/brainswarms/' + idea_id
     })
       .done(function (brainswarmsData) {
-        // var brainswarms = this._brainswarms;
-        // for (var i = 0; i< brainswarmsData.length; i++){
-        //   this._brainswarms.push(brainswarmsData[i]);
-        //   if (brainswarmsData[i].idea === idea_id){
-        //     var tempBrainswarm = brainswarmsData[i];
-        //   }
-        // }
-        // socket.emit('brainswarm-change', this._brainswarms);
-        console.log("correct brainswarm", brainswarmsData);
+
         this.trigger();
         if (brainswarmsData) {
           return callback(brainswarmsData[0]);
-          // return tempBrainswarm;
         };
         callback();
       }.bind(this))
@@ -63,15 +25,6 @@ var BrainswarmStore = Reflux.createStore({
 
   },
 
-  // findBrainswarm: function(brainswarmId) {
-  //   var brainswarms = this._brainswarms;
-  //   for (var i =0; i< brainswarms.length; i++){
-  //      if (brainswarms[i]._id === brainswarmId){
-  //        return brainswarms[i];
-  //      }
-  //   }
-  // },
-
   getBrainswarmById: function(brainswarm_id, callback) {
     // This ajax call sends the brainswarm_id as an "idea_id" to the routes
     $.ajax({
@@ -79,8 +32,7 @@ var BrainswarmStore = Reflux.createStore({
       url: "/brainswarms/" + brainswarm_id
     })
       .done(function(brainswarmData){
-        // this._brainswarms.push(brainswarmData);
-        // console.log("brainswarm by brainswarmID", brainswarmData);
+
         this.trigger();
         callback(brainswarmData[0]);
       }.bind(this))
@@ -96,12 +48,7 @@ var BrainswarmStore = Reflux.createStore({
       data: {name: name}
     })
       .done(function(brainswarm) {
-        // this._brainswarms.push(brainswarm);
-        // _brainswarms[brainswarm._id] = brainswarm
 
-        // broadcast that _rooms has changed
-        // socket.emit('brainswarm-change', this._brainswarms);
-        console.log("created brainswarm", brainswarm);
         this.trigger();
         callback(brainswarm._id);
       }.bind(this))
@@ -119,12 +66,6 @@ var BrainswarmStore = Reflux.createStore({
     })
       .done(function(brainswarmEdit) {
 
-        // for (var i = 0; i < this._brainswarms.length; i++){
-        //  if (this._brainswarms[i]._id === brainswarmEdit._id){
-        //    this._brainswarms[i].map = brainswarmEdit.map
-        //  }
-        // };
-        console.log("edited brainswarm", brainswarmEdit);
         this.trigger();
       }.bind(this))
       .fail(function(error) {
